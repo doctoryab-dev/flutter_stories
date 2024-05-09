ca79bdf7b7e4b4d3e371d6fe73b3d3de762188bd
doctoryab-dev <doctoryab.dev@gmail.com>
2024-05-09 21:30:02 +0200

    init

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<stories_for_flutter/StoriesForFlutterPlugin.h>)
+#import <stories_for_flutter/StoriesForFlutterPlugin.h>
+#else
+@import stories_for_flutter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [StoriesForFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"StoriesForFlutterPlugin"]];
+}
+
+@end
---
+++ example/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,19 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\stories_for_flutter\\example" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=F:\\stories_for_flutter\\example"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\stories_for_flutter\\example\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\stories_for_flutter\\example"
+  "FLUTTER_TARGET=F:\\stories_for_flutter\\example\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\stories_for_flutter\\example\\.dart_tool\\package_config.json"
+)
